4a73cb2308fc664c6dfb45220bb10ab61b224bda
DomDev126 <dom.wolf126@gmail.com>
2018-10-29 21:57:36 +0800

    Fixed deprecated errors in test App (task #8244)

--- tests/config/routes.php
+++ tests/config/routes.php
@@ -5,10 +5,4 @@ use Cake\Core\Configure;
 use Cake\Core\Plugin;
 use Cake\Routing\Router;
 
-/**
- * Load all plugin routes.  See the Plugin documentation on
- * how to customize the loading of plugin routes.
- */
-Plugin::routes();
-
 Router::connect('/users/login', ['controller' => 'Users', 'action' => 'login']);
