c47aec89f2ebabffca15ba236e669298a86f8059
DomDev126 <dom.wolf126@gmail.com>
2018-10-30 11:19:18 +0800

    PHPStan fixes (task #8244)

--- tests/App/Controller/UsersController.php
+++ tests/App/Controller/UsersController.php
@@ -1,12 +1,14 @@
 <?php
 namespace Foobar\Test\App\Controller;
 
-use \Cake\Controller\Controller;
+use Cake\Controller\Controller;
 
 class UsersController extends Controller
 {
     /**
      * Simple users login action
+     *
+     * @return void
      */
     public function login()
     {
