65636c1e5b5b579319bf24e58ee245a3060132a6
DomDev126 <dom.wolf126@gmail.com>
2019-04-29 14:18:23 +0800

    Add tests for BooleanParamConverter class

---
+++ tests/TestCase/BooleanParamConverterTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ParamConverter\Test\TestCase;
+
+use Cake\Http\Exception\BadRequestException;
+use Cake\TestSuite\TestCase;
+use ParamConverter\BooleanParamConverter;
+
+class BooleanParamConverterTest extends TestCase
+{
+    public function testSupports()
+    {
+        $converter = new BooleanParamConverter();
+        $this->assertTrue($converter->supports('bool'));
+        $this->assertFalse($converter->supports('int'));
+    }
+
+    /**
+     * @dataProvider conversionDateProvider
+     */
+    public function testConvertTo($rawValue, $convertedValue)
+    {
+        $converter = new BooleanParamConverter();
+        $this->assertEquals($convertedValue, $converter->convertTo($rawValue, "bool"));
+
+    }
+
+    public function testException()
+    {
+        $converter = new BooleanParamConverter();
+        $this->expectException(BadRequestException::class);
+        $converter->convertTo("not-a-bool", "bool");
+    }
+
+    public function conversionDateProvider()
+    {
+        return [
+            // raw value, converted value
+            ['1', true],
+            ['0', false],
+            ['true', true],
+            ['false', false],
+            ['yes', true],
+            ['no', false],
+            ['on', true],
+            ['off', false],
+        ];
+    }
+}
